0a6f63554c69044c5d4e15eeb8edb7eb4f163dce
Alain Amstutz <131078877+alainamstutz@users.noreply.github.com>
2023-11-28 14:53:46 +0000

    Metfin codelist

--- analysis/codelists.py
+++ analysis/codelists.py
@@ -1,149 +1,33 @@
-from cohortextractor import codelist, codelist_from_csv, combine_codelists
+from ehrql import (
+    case,
+    codelist_from_csv,
+    create_dataset,
+    days,
+    when,
+)
+#from ehrql.tables.beta.core import clinical_events, patients
 
-covid_codes = codelist_from_csv(
-    "codelists/opensafely-covid-identification.csv",
-    system="icd10",
-    column="icd10_code",
-)
-covid_primary_care_positive_test = codelist_from_csv(
-    "codelists/opensafely-covid-identification-in-primary-care-probable-covid-positive-test.csv",
-    system="ctv3",
-    column="CTV3ID",
-)
-covid_primary_care_code = codelist_from_csv(
-    "codelists/opensafely-covid-identification-in-primary-care-probable-covid-clinical-code.csv",
-    system="ctv3",
-    column="CTV3ID",
-)
-covid_primary_care_sequalae = codelist_from_csv(
-    "codelists/opensafely-covid-identification-in-primary-care-probable-covid-sequelae.csv",
-    system="ctv3",
-    column="CTV3ID",
-)
-any_primary_care_code = combine_codelists(
-    covid_primary_care_code,
-    covid_primary_care_positive_test,
-    covid_primary_care_sequalae,
-)
-long_covid_diagnostic_codes = codelist_from_csv(
-    "codelists/opensafely-nice-managing-the-long-term-effects-of-covid-19.csv",
-    system="snomed",
-    column="code",
-)
-long_covid_referral_codes = codelist_from_csv(
-    "codelists/opensafely-referral-and-signposting-for-long-covid.csv",
-    system="snomed",
-    column="code",
-)
-long_covid_assessment_codes = codelist_from_csv(
-    "codelists/opensafely-assessment-instruments-and-outcome-measures-for-long-covid.csv",
-    system="snomed",
-    column="code",
-)
-any_long_covid_code = combine_codelists(
-    long_covid_diagnostic_codes, long_covid_referral_codes, long_covid_assessment_codes
-)
-post_viral_fatigue_codes = codelist_from_csv(
-    "codelists/user-alex-walker-post-viral-syndrome.csv",
-    system="snomed",
-    column="code",
-)
-ethnicity_codes = codelist_from_csv(
+covid_codes = codelist_from_csv("codelists/opensafely-covid-identification.csv", column="icd10_code")
+covid_primary_care_positive_test = codelist_from_csv("codelists/opensafely-covid-identification-in-primary-care-probable-covid-positive-test.csv", column="CTV3ID")
+covid_primary_care_code = codelist_from_csv("codelists/opensafely-covid-identification-in-primary-care-probable-covid-clinical-code.csv", column="CTV3ID")
+covid_primary_care_sequalae = codelist_from_csv("codelists/opensafely-covid-identification-in-primary-care-probable-covid-sequelae.csv", column="CTV3ID")
+
+long_covid_diagnostic_codes = codelist_from_csv("codelists/opensafely-nice-managing-the-long-term-effects-of-covid-19.csv",column="code")
+long_covid_referral_codes = codelist_from_csv("codelists/opensafely-referral-and-signposting-for-long-covid.csv",column="code")
+long_covid_assessment_codes = codelist_from_csv("codelists/opensafely-assessment-instruments-and-outcome-measures-for-long-covid.csv",column="code")
+
+post_viral_fatigue_codes = codelist_from_csv("codelists/user-alex-walker-post-viral-syndrome.csv",column="code")
+diabetes_codes = codelist_from_csv( "codelists/opensafely-diabetes.csv", column="CTV3ID")
+
+ethnicity_codelist = codelist_from_csv(
     "codelists/opensafely-ethnicity.csv",
-    system="ctv3",
     column="Code",
     category_column="Grouping_6",
 )
-dementia_codes = codelist_from_csv(
-    "codelists/opensafely-dementia.csv", system="ctv3", column="CTV3ID"
-)
-other_neuro_codes = codelist_from_csv(
-    "codelists/opensafely-other-neurological-conditions.csv",
-    system="ctv3",
-    column="CTV3ID",
-)
-chronic_respiratory_disease_codes = codelist_from_csv(
-    "codelists/opensafely-chronic-respiratory-disease.csv",
-    system="ctv3",
-    column="CTV3ID",
-)
-asthma_codes = codelist_from_csv(
-    "codelists/opensafely-asthma-diagnosis.csv", system="ctv3", column="CTV3ID"
-)
-salbutamol_codes = codelist_from_csv(
-    "codelists/opensafely-asthma-inhaler-salbutamol-medication.csv",
-    system="snomed",
-    column="id",
-)
-ics_codes = codelist_from_csv(
-    "codelists/opensafely-asthma-inhaler-steroid-medication.csv",
-    system="snomed",
-    column="id",
-)
-prednisolone_codes = codelist_from_csv(
-    "codelists/opensafely-asthma-oral-prednisolone-medication.csv",
-    system="snomed",
-    column="snomed_id",
-)
-clear_smoking_codes = codelist_from_csv(
-    "codelists/opensafely-smoking-clear.csv",
-    system="ctv3",
-    column="CTV3Code",
-    category_column="Category",
-)
-stroke_gp_codes = codelist_from_csv(
-    "codelists/opensafely-stroke-updated.csv", system="ctv3", column="CTV3ID"
-)
-lung_cancer_codes = codelist_from_csv(
-    "codelists/opensafely-lung-cancer.csv", system="ctv3", column="CTV3ID"
-)
-haem_cancer_codes = codelist_from_csv(
-    "codelists/opensafely-haematological-cancer.csv", system="ctv3", column="CTV3ID"
-)
-other_cancer_codes = codelist_from_csv(
-    "codelists/opensafely-cancer-excluding-lung-and-haematological.csv",
-    system="ctv3",
-    column="CTV3ID",
-)
-chronic_cardiac_disease_codes = codelist_from_csv(
-    "codelists/opensafely-chronic-cardiac-disease.csv", system="ctv3", column="CTV3ID"
-)
-hiv_codes = codelist_from_csv(
-    "codelists/opensafely-hiv.csv",
-    system="ctv3",
-    column="CTV3ID",
-    category_column="CTV3ID",
-)
-permanent_immune_codes = codelist_from_csv(
-    "codelists/opensafely-permanent-immunosuppression.csv",
-    system="ctv3",
-    column="CTV3ID",
-)
-temp_immune_codes = codelist_from_csv(
-    "codelists/opensafely-temporary-immunosuppression.csv",
-    system="ctv3",
-    column="CTV3ID",
-)
-aplastic_codes = codelist_from_csv(
-    "codelists/opensafely-aplastic-anaemia.csv", system="ctv3", column="CTV3ID"
-)
-spleen_codes = codelist_from_csv(
-    "codelists/opensafely-asplenia.csv", system="ctv3", column="CTV3ID"
-)
-organ_transplant_codes = codelist_from_csv(
-    "codelists/opensafely-solid-organ-transplantation.csv",
-    system="ctv3",
-    column="CTV3ID",
-)
-sickle_cell_codes = codelist_from_csv(
-    "codelists/opensafely-sickle-cell-disease.csv", system="ctv3", column="CTV3ID"
-)
-ra_sle_psoriasis_codes = codelist_from_csv(
-    "codelists/opensafely-ra-sle-psoriasis.csv", system="ctv3", column="CTV3ID"
-)
-chronic_liver_disease_codes = codelist_from_csv(
-    "codelists/opensafely-chronic-liver-disease.csv", system="ctv3", column="CTV3ID"
-)
-diabetes_codes = codelist_from_csv(
-    "codelists/opensafely-diabetes.csv", system="ctv3", column="CTV3ID"
-)
+
+asthma_inhaler_codelist = codelist_from_csv("codelists/opensafely-asthma-inhaler-salbutamol-medication.csv",column="code")
+metformin_codelist = codelist_from_csv("codelist/user-john-tazare-metformin-dmd.csv",column="code")
+
+
+#dataset = create_dataset()
+#dataset.define_population(patients.exists_for_patient())
--- analysis/dataset_definition.py
+++ analysis/dataset_definition.py
@@ -12,57 +12,68 @@ from ehrql.tables.beta.tpp import (
     medications,
     patients,
     practice_registrations,
+    ons_deaths,
 )
 
-index_date = "2023-10-01"
+# define important dates
+pandemic_start = "2020-02-01"
+index_date = "2021-12-16" ### Omicron BA.1 dominance in UK
 
+# start the dataset and set the dummy dataset size
 dataset = create_dataset()
+dataset.configure_dummy_data(population_size=50)
 
-dataset.configure_dummy_data(population_size=10)
-
-# codelists
-
-ethnicity_codelist = codelist_from_csv(
-    "codelists/opensafely-ethnicity.csv",
-    column="Code",
-    category_column="Grouping_6",
+# import codelists from codelists.py
+from codelists import (
+    long_covid_diagnostic_codes, 
+    long_covid_referral_codes, 
+    long_covid_assessment_codes, 
+    post_viral_fatigue_codes, 
+    asthma_inhaler_codelist,
+    ethnicity_codelist,
+    metformin_codelist
 )
 
-asthma_inhaler_codelist = codelist_from_csv(
-    "codelists/opensafely-asthma-inhaler-salbutamol-medication.csv",
-    column="code",
-)
-
-# population variables
-
-is_female_or_male = patients.sex.is_in(["female", "male"])
-
+# population variables for dataset definition
+is_female_or_male = patients.sex.is_in(["female", "male"]) # only include f, m and no missing values
 was_adult = (patients.age_on(index_date) >= 18) & (
     patients.age_on(index_date) <= 110
-)
-
+) # only include adults and no missing values
 was_alive = (
     patients.date_of_death.is_after(index_date)
     | patients.date_of_death.is_null()
-)
-
+) # only include if alive 
 was_registered = practice_registrations.for_patient_on(
     index_date
-).exists_for_patient()
+).exists_for_patient() # only include if registered on index date
 
+# define/create dataset
 dataset.define_population(
     is_female_or_male
     & was_adult
     & was_alive
     & was_registered
+) 
+
+
+# DEMOGRAPHIC variables
+## age
+age = patients.age_on(index_date)
+dataset.age = age
+dataset.age_band = case(
+        when(age < 20).then("0-19"),
+        when(age < 40).then("20-39"),
+        when(age < 60).then("40-59"),
+        when(age < 80).then("60-79"),
+        when(age >= 80).then("80+"),
+        default="missing",
 )
+dataset.date_of_birth = patients.date_of_birth
 
-# demographic variables
-
-dataset.age = patients.age_on(index_date)
-
+## sex
 dataset.sex = patients.sex
 
+## ethnicitiy
 dataset.ethnicity = (
     clinical_events.where(
         clinical_events.ctv3_code.is_in(ethnicity_codelist)
@@ -72,19 +83,44 @@ dataset.ethnicity = (
     .ctv3_code.to_category(ethnicity_codelist)
 )
 
-imd_rounded = addresses.for_patient_on(
-    index_date
-).imd_rounded
-max_imd = 32844
+## geographic and IMD data (https://www.gov.uk/government/statistics/english-indices-of-deprivation-2019)
+address = addresses.for_patient_on(index_date)
+dataset.imd = address.imd_rounded
 dataset.imd_quintile = case(
-    when(imd_rounded < int(max_imd * 1 / 5)).then(1),
-    when(imd_rounded < int(max_imd * 2 / 5)).then(2),
-    when(imd_rounded < int(max_imd * 3 / 5)).then(3),
-    when(imd_rounded < int(max_imd * 4 / 5)).then(4),
-    when(imd_rounded <= max_imd).then(5),
+    when((address.imd_rounded >=0) & (address.imd_rounded < int(32844 * 1 / 5))).then("1 (most deprived)"),
+    when(address.imd_rounded < int(32844 * 2 / 5)).then("2"),
+    when(address.imd_rounded < int(32844 * 3 / 5)).then("3"),
+    when(address.imd_rounded < int(32844 * 4 / 5)).then("4"),
+    when(address.imd_rounded < int(32844 * 5 / 5)).then("5 (least deprived)"),
+    default="unknown"
 )
+dataset.rural_urban_classification = address.rural_urban_classification
+#1 - Urban major conurbation
+#2 - Urban minor conurbation
+#3 - Urban city and town
+#4 - Urban city and town in a sparse setting
+#5 - Rural town and fringe
+#6 - Rural town and fringe in a sparse setting
+#7 - Rural village and dispersed
+#8 - Rural village and dispersed in a sparse setting
+dataset.msoa_code = address.msoa_code
 
-# exposure variables
+## patient's practice data
+registration = practice_registrations.for_patient_on(index_date)
+dataset.practice = registration.practice_pseudo_id
+dataset.stp = registration.practice_stp
+dataset.region = registration.practice_nuts1_region_name
+
+
+
+# EXPOSURE variables
+dataset.first_metfin = (
+    medications.where(
+        medications.dmd_code.is_in(metformin_codelist))
+        .where(medications.date.is_on_or_between(index_date, "2023-01-01"))
+        .medications.sort_by(medications.date)
+        .first_for_patient()
+)
 
 dataset.num_asthma_inhaler_medications = medications.where(
     medications.dmd_code.is_in(asthma_inhaler_codelist)
@@ -93,15 +129,52 @@ dataset.num_asthma_inhaler_medications = medications.where(
     )
 ).count_for_patient()
 
-# outcome variables
+# OUTCOME variables
+## long/post covid
+dataset.long_covid = (
+    clinical_events.where(
+        clinical_events.snomedct_code.is_in(long_covid_diagnostic_codes))
+        .where(clinical_events.date.is_on_or_after(pandemic_start))
+        .sort_by(clinical_events.date)
+        .first_for_patient()
+        .exists_for_patient()
+        |
+    clinical_events.where(
+        clinical_events.snomedct_code.is_in(long_covid_referral_codes))
+        .where(clinical_events.date.is_on_or_after(pandemic_start))
+        .sort_by(clinical_events.date)
+        .first_for_patient()
+        .exists_for_patient()  
+        |
+    clinical_events.where(
+        clinical_events.snomedct_code.is_in(long_covid_assessment_codes))
+        .where(clinical_events.date.is_on_or_after(pandemic_start))
+        .sort_by(clinical_events.date)
+        .first_for_patient()
+        .exists_for_patient() 
+)
+dataset.post_viral_fatigue = (
+    clinical_events.where(
+        clinical_events.snomedct_code.is_in(post_viral_fatigue_codes))
+        .where(clinical_events.date.is_on_or_after(pandemic_start))
+        .sort_by(clinical_events.date)
+        .first_for_patient()
+        .date
+)
 
+## hospital admission
 dataset.date_of_first_admission = (
     hospital_admissions.where(
         hospital_admissions.admission_date.is_after(
-            index_date
+            pandemic_start
         )
     )
     .sort_by(hospital_admissions.admission_date)
     .first_for_patient()
     .admission_date
 )
+
+## death
+dataset.date_of_death_ons = ons_deaths.date
+dataset.place_of_death_ons = ons_deaths.place
+dataset.cause_of_death_ons = ons_deaths.cause_of_death_01
